ee12300d826a3f6dbfabd7a6b04aa973229e6da3
NoorFatimaAnwar <noorfatima0161@gmail.com>
2025-10-02 07:26:31 -0700

    add notebok folder, logging and exception

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=False)
+
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    format="[%(asctime)s] %(lineNo)d %(levelname)s - %(name)s - %(message)s",
+    level=logging.INFO
+)
+ 
+
+
